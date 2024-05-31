ea86b46965b2636b188521f55e16791b1df17373
zoë ૮ ˶ᵔ ᵕ ᵔ˶ ა <48801280+ZoeAkpan@users.noreply.github.com>
2024-05-31 15:42:24 -0400

    Update and rename main to main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ file = "/Users/zoeakpan/Desktop/personal projects/Hello [sound effect].mp3"
 
 #easy to change file
 def transcribing(file):
+# no way the api key should be shared but i dont want to create an env folder
     openai.api_key = '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'
     audio = open(file, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio)
